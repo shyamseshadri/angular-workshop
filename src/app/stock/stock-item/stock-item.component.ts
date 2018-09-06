600d6595c158d26e194a9cac09ee5ebac3f421b2
Shyam Seshadri <shyamseshadri@gmail.com>
2018-09-06 09:17:36 +0530

    S1-7: Using class specific binding

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -14,15 +14,6 @@ export class StockItemComponent implements OnInit {
 
   ngOnInit() {
     this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
-
-    let diff = (this.stock.price / this.stock.previousPrice) - 1;
-    let largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      "positive": this.stock.isPositiveChange(),
-      "negative": !this.stock.isPositiveChange(),
-      "large-change": largeChange,
-      "small-change": !largeChange
-    };
   }
 
   toggleFavourite() {
