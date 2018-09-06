4b36212a283f68fce719d5da778e4434260881c0
Shyam Seshadri <shyamseshadri@gmail.com>
2018-09-06 08:51:50 +0530

    S1-3: Property binding at play

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -10,6 +10,7 @@ export class StockItemComponent implements OnInit {
   public code: string;
   public price: number;
   public previousPrice: number;
+  public positiveChange: boolean;
 
   constructor() { }
 
@@ -18,6 +19,7 @@ export class StockItemComponent implements OnInit {
     this.code = 'TSC';
     this.price = 85;
     this.previousPrice = 80;
+    this.positiveChange = this.price >= this.previousPrice;
   }
 
 }
