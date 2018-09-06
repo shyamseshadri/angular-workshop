6af76c5bef256527a9d9f98f8c472be56c7786ef
Shyam Seshadri <shyamseshadri@gmail.com>
2018-09-06 09:14:14 +0530

    S1-6: Using ngClass binding

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -8,11 +8,21 @@ import { Stock } from '../../model/stock';
 })
 export class StockItemComponent implements OnInit {
   public stock: Stock;
+  public stockClasses: any;
 
   constructor() { }
 
   ngOnInit() {
     this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+
+    let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    let largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+      "positive": this.stock.isPositiveChange(),
+      "negative": !this.stock.isPositiveChange(),
+      "large-change": largeChange,
+      "small-change": !largeChange
+    };
   }
 
   toggleFavourite() {
