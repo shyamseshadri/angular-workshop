ab69027f66748299a4d99d90a4ff825995a0dc5e
Shyam Seshadri <shyamseshadri@gmail.com>
2018-09-07 12:28:55 +0530

    S2-3: Complete working form

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -9,9 +9,20 @@ import { Stock } from '../../model/stock';
 export class CreateStockComponent implements OnInit {
 
   public stock: Stock;
+  public confirmed:boolean = false;
   constructor() {
     this.stock = new Stock('test', '', 0, 0, 'NASDAQ');
   }
 
   ngOnInit() { }
+
+  onPriceChange(newPrice) {
+    this.stock.price = newPrice;
+    this.stock.previousPrice = newPrice;
+  }
+
+  createStock() {
+    console.log('Stock being created with', this.stock);
+  }
+
 }
