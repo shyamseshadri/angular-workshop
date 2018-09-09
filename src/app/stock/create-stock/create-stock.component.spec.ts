bedb0a7913e1f5d103327b3e133721b12ca86d04
Shyam Seshadri <shyamseshadri@gmail.com>
2018-09-09 11:48:54 +0530

    S4-2: Angular aware test for create stock component

--- src/app/stock/create-stock/create-stock.component.spec.ts
+++ src/app/stock/create-stock/create-stock.component.spec.ts
@@ -1,9 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick } from '@angular/core/testing';
 
 import { CreateStockComponent } from './create-stock.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { StockService } from '../../services/stock.service';
+import { By } from '@angular/platform-browser';
 
 describe('CreateStockComponent', () => {
   let component: CreateStockComponent;
@@ -18,13 +19,30 @@ describe('CreateStockComponent', () => {
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(CreateStockComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    fixture.whenStable().then(() => {});
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should have form elements', async(() => {
+    const nameEl = fixture.debugElement.query(By.css('.stock-name input'));
+    expect(nameEl.nativeElement.value).toEqual('');
+
+    let submitBtn = fixture.debugElement.query(By.css('.form-group button'));
+    expect(submitBtn.nativeElement.disabled).toBeTruthy();
+    let confirmCheckbox = fixture.debugElement.query(By.css('.stock-confirm input'));
+    expect(confirmCheckbox.nativeElement.checked).toBe(false);
+    expect(component.confirmed).toBe(false);
+    confirmCheckbox.nativeElement.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(confirmCheckbox.nativeElement.checked).toBe(true);
+
+      expect(component.confirmed).toBe(true);
+      submitBtn = fixture.debugElement.query(By.css('.form-group button'));
+      expect(submitBtn.nativeElement.disabled).toBe(false);
+    });
+  }));
 });
