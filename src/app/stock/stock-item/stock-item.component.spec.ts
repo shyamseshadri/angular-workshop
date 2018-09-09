259f5c22dc8f9b63c810a1bfbc5b0bfab1ff8a18
Shyam Seshadri <shyamseshadri@gmail.com>
2018-09-09 11:11:27 +0530

    S4-1: Isolated Unit Test

--- src/app/stock/stock-item/stock-item.component.spec.ts
+++ src/app/stock/stock-item/stock-item.component.spec.ts
@@ -6,6 +6,29 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { StockService } from '../../services/stock.service';
 
+describe('StockItemComponent isolated test', () => {
+  it('should look at stock to decide whether it is positive change', () => {
+    let component = new StockItemComponent(null);
+
+    // By Default
+    expect(component.isPositiveChange()).toBeFalsy();
+
+    component.stock = {
+      code: 'Test',
+      previousPrice: 40,
+      price: 50,
+      exchange: 'TEST',
+      favourite: false,
+      name: 'TEsting Product'
+    };
+
+    expect(component.isPositiveChange()).toBeTruthy();
+
+    component.stock.previousPrice = 60;
+    expect(component.isPositiveChange()).toBeFalsy();
+  });
+});
+
 describe('StockItemComponent', () => {
   let component: StockItemComponent;
   let fixture: ComponentFixture<StockItemComponent>;
